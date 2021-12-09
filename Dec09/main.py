74d52e1a7a881cb792a474824d32016bf5d8ca79
January Shen <january.shen@tupl.com>
2021-12-09 09:08:23 -0800

    Dec09 DFS solution from Reddit

--- Dec09/main.py
+++ Dec09/main.py
@@ -15,13 +15,13 @@ def get_matrix(inputData):
 
 def get_center_low_points(matrix):
     counter = 0
-    for row in range(1, matrix.shape[0]-1):
-        for col in range(1, matrix.shape[1]-1):
+    for row in range(1, matrix.shape[0] - 1):
+        for col in range(1, matrix.shape[1] - 1):
             self = matrix.item((row, col))
-            up = matrix[row-1, col]
-            down = matrix[row+1, col]
-            left = matrix[row, col-1]
-            right = matrix[row, col+1]
+            up = matrix[row - 1, col]
+            down = matrix[row + 1, col]
+            left = matrix[row, col - 1]
+            right = matrix[row, col + 1]
             if self < min(up, down, left, right):
                 counter += self + 1
     return counter
@@ -32,7 +32,7 @@ def get_edge_low_points(matrix):
 
     # top row
     row = 0
-    for col in range(1, matrix.shape[1]-1):
+    for col in range(1, matrix.shape[1] - 1):
         self = matrix.item((row, col))
         down = matrix[row + 1, col]
         left = matrix[row, col - 1]
@@ -42,8 +42,8 @@ def get_edge_low_points(matrix):
             counter += self + 1
 
     # bottom row
-    row = matrix.shape[0]-1
-    for col in range(1, matrix.shape[1]-1):
+    row = matrix.shape[0] - 1
+    for col in range(1, matrix.shape[1] - 1):
         self = matrix.item((row, col))
         up = matrix[row - 1, col]
         left = matrix[row, col - 1]
@@ -53,7 +53,7 @@ def get_edge_low_points(matrix):
 
     # most left col
     col = 0
-    for row in range(1, matrix.shape[0]-1):
+    for row in range(1, matrix.shape[0] - 1):
         self = matrix.item((row, col))
         up = matrix[row - 1, col]
         down = matrix[row + 1, col]
@@ -63,7 +63,7 @@ def get_edge_low_points(matrix):
 
     # most right col
     col = matrix.shape[1] - 1
-    for row in range(1, matrix.shape[0]-1):
+    for row in range(1, matrix.shape[0] - 1):
         self = matrix.item((row, col))
         up = matrix[row - 1, col]
         down = matrix[row + 1, col]
@@ -72,8 +72,6 @@ def get_edge_low_points(matrix):
             counter += self + 1
 
     return counter
-
-
 
 
 def part1(inputData):
@@ -107,12 +105,12 @@ def part2(inputData):
             return island_num
 
         def dfs(self, matrix, row, col, island_size):
-            direction = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            direction = [[1, 0], [0, -1], [0, 1], [-1, 0]]
             matrix[row][col] = 0
             for dir in direction:
                 nr = row + dir[0]
                 nc = col + dir[1]
-                if 0 < nr < matrix.shape[0] and 0 < nc < matrix.shape[1]:
+                if 0 <= nr < matrix.shape[0] and 0 <= nc < matrix.shape[1]:
                     if matrix[nr][nc] == 1:
                         island_size += 1
                         self.dfs(matrix, nr, nc, island_size)
@@ -125,7 +123,6 @@ def part2(inputData):
                 self.island_2 = island_size
             elif island_size > self.island_3:
                 self.island_3 = island_size
-            print(island_size)
 
     sol = Solution()
     print(sol.num_of_basin(non_nine_matrix))
@@ -140,4 +137,35 @@ def part2(inputData):
 if __name__ == "__main__":
     input_data = "./data.txt"
     # part1(input_data)
-    part2(input_data)
+    # part2(input_data)
+
+    # Part 2: Modified input from Reddit user lbm364dl at https://www.reddit.com/r/adventofcode/comments/rca6vp/2021_day_9_solutions/
+    grid = [[*map(int, l[:-1])] for l in open('./data.txt')]
+    delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # order does not matter
+    n, m = len(grid), len(grid[0])
+    visited = set()
+
+
+    def is_outside(y, x):
+        return not (0 <= y < n and 0 <= x < m)
+
+
+    def is_low_point(y, x):
+        return all(is_outside(y + dy, x + dx) or grid[y + dy][x + dx] > grid[y][x] for dy, dx in delta)
+
+
+    def dfs(y, x):
+        if is_outside(y, x) or grid[y][x] == 9 or (y, x) in visited:
+            return 0
+
+        visited.add((y, x))
+        return 1 + sum(dfs(y + dy, x + dx) for dy, dx in delta)
+
+
+    lows = [(y, x) for y in range(n-1) for x in range(m-1) if is_low_point(y, x)]
+
+    basins = sorted([dfs(y, x) for y, x in lows])
+
+    # print('Star 1:', sum(grid[y][x] + 1 for y, x in lows))
+    print('Star 2:', basins[-1] * basins[-2] * basins[-3])
+    print(basins[-1], basins[-2], basins[-3])
