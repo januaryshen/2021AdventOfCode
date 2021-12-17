dc981bd198595ff8ddf3a8d76f42d8011b678c6e
January Shen <january.shen@tupl.com>
2021-12-17 01:31:33 -0800

    Dec 16 part 1 was done but now gone

--- Dec15/main.py
+++ Dec15/main.py
@@ -27,33 +27,33 @@ def dijkstra(adjacent, start, end):
 
 
 def part1(inputData):
-        board = list(parse(open(inputData)))
-        m = len(board)
-        n = len(board[0])
+    board = list(parse(open(inputData)))
+    m = len(board)
+    n = len(board[0])
 
-        def adjacent(u):
-            i, j = u
-            for ii, jj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
-                if 0 <= ii < m and 0 <= jj < n:
-                    yield (ii, jj), board[ii][jj]
+    def adjacent(u):
+        i, j = u
+        for ii, jj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+            if 0 <= ii < m and 0 <= jj < n:
+                yield (ii, jj), board[ii][jj]
 
-        result = dijkstra(adjacent, (0, 0), (m - 1, n - 1))
-        print(result)
+    result = dijkstra(adjacent, (0, 0), (m - 1, n - 1))
+    print(result)
 
 
 def part2(inputData):
     board = list(parse(open(inputData)))
     mm, nn = len(board), len(board[0])
-    m, n = 5*mm, 5*nn
+    m, n = 5 * mm, 5 * nn
 
-    def add_wrap( c, idx):
+    def add_wrap(c, idx):
         return (c - 1 + idx) % 9 + 1
 
     def adjacent(u):
         i, j = u
-        for ii, jj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+        for ii, jj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
             if 0 <= ii < m and 0 <= jj < n:
-                weight = add_wrap( board[ii%mm][jj%nn], ii//mm + jj//nn)
+                weight = add_wrap(board[ii % mm][jj % nn], ii // mm + jj // nn)
                 yield (ii, jj), weight
 
     result = dijkstra(adjacent, (0, 0), (m - 1, n - 1))
@@ -66,4 +66,3 @@ if __name__ == "__main__":
     part2(input_data)
 
     # source: Zealousideal-Pen9091 from https://www.reddit.com/r/adventofcode/comments/rgqzt5/2021_day_15_solutions/
-
---
+++ Dec16/main.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+
+class BITS:
+    def __init__(self, hex_string):
+        self.hex_string = hex_string
+        self.hex_size = len(self.hex_string) * 4
+        self.binary_string = bin(int(hex_string, 16))[2:].zfill(self.hex_size)
+        self.version_sum = 0
+        self.literal_value = 0
+        self.subpacket_value = []
+        self.subpacket_bit = 0
+        self.typeID = 0
+        self.literal_value_subpacket = []
+        self.ultimate_sum = 0
+
+    def get_literal_value(self, binary_string):
+        max_group = len(binary_string) // 5
+        value = ""
+        for i in range(max_group):
+            target = binary_string[i*5: (i+1)*5]
+            value = value + target[1:]
+            if target[0] == "0":
+                self.literal_value += int(value, 2)
+                self.literal_value_subpacket.append(int(value, 2))
+                self.binary_string = binary_string[(i+1)*5:]
+                self.subpacket_bit += (i + 1) * 5
+                self.get_version_and_type(self.binary_string)
+                break
+
+    def get_version_decimal(self, binary_string):
+        self.version_sum += int(binary_string, 2)
+
+    def num_of_packet_mode(self, binary_string):
+        # next 11 bits contains the number of packet
+        num_of_packet = int(binary_string[0: 11], 2)
+        self.binary_string = binary_string[11:]
+        self.subpacket_bit += 11
+        for i in range(num_of_packet):
+            self.get_version_and_type(self.binary_string)
+
+    def length_in_bit_mode(self, binary_string):
+        # next 15 bits contains the bit usage of subpacket
+        self.subpacket_value.append(int(binary_string[0: 15], 2))
+        self.binary_string = binary_string[15:]
+        self.subpacket_bit += 15
+        while True:
+            self.get_version_and_type(self.binary_string)
+            if self.subpacket_bit < self.subpacket_value[-1]:
+                break
+
+        self.subpacket_value.pop()
+
+    def sum_ultimate(self):
+        if self.literal_value_subpacket:
+            if self.typeID == 0:
+                self.ultimate_sum += np.sum(self.literal_value_subpacket)
+            elif self.typeID == 1:
+                if len(self.literal_value_subpacket) == 1:
+                    self.ultimate_sum += np.sum(self.literal_value_subpacket)
+                else:
+                    self.ultimate_sum += np.prod(self.literal_value_subpacket)
+            elif self.typeID == 2:
+                self.ultimate_sum += np.min(self.literal_value_subpacket)
+            elif self.typeID == 3:
+                self.ultimate_sum += np.max(self.literal_value_subpacket)
+            elif self.typeID == 5 and len(self.literal_value_subpacket) == 2:
+                self.ultimate_sum += 1 if self.literal_value_subpacket[0] > self.literal_value_subpacket[1] else 0
+            elif self.typeID == 6 and len(self.literal_value_subpacket) == 2:
+                self.ultimate_sum += 1 if self.literal_value_subpacket[0] < self.literal_value_subpacket[1] else 0
+            elif self.typeID == 7 and len(self.literal_value_subpacket) == 2:
+                self.ultimate_sum += 1 if self.literal_value_subpacket[0] == self.literal_value_subpacket[1] else 0
+        self.literal_value_subpacket = []
+
+    def get_version_and_type(self, binary_string):
+        # self.sum_ultimate()
+        if len(self.binary_string) >= 11:
+            self.get_version_decimal(binary_string[0:3])
+            if int(binary_string[3:6], 2) == 4:
+                self.subpacket_bit += 6
+                self.get_literal_value(binary_string[6:])
+            else:  # operator mode
+                self.typeID = int(binary_string[3:6], 2)
+                self.subpacket_bit += 7
+                if binary_string[6] == '1':
+                    self.num_of_packet_mode(binary_string[7:])
+                else:
+                    self.length_in_bit_mode(binary_string[7:])
+
+
+def part1(inputData):
+    packet = BITS(open(inputData, 'r').read().rstrip())
+    packet.get_version_and_type(packet.binary_string)
+    print("literal value: ", packet.literal_value)
+    print("version sum: ", packet.version_sum)
+
+
+def part2(inputData):
+    packet = BITS(open(inputData, 'r').read().rstrip())
+    packet.get_version_and_type(packet.binary_string)
+    print("literal value: ", packet.literal_value)
+    print("version sum: ", packet.version_sum)
+    print("ultimate sum: ", packet.ultimate_sum)
+
+
+if __name__ == "__main__":
+    input_data = "./data.txt"
+    part1(input_data)
+    # part2(input_data)
+
+    # source: Zealousideal-Pen9091 from https://www.reddit.com/r/adventofcode/comments/rgqzt5/2021_day_15_solutions/
